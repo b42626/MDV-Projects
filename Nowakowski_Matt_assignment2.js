4d156114fe53c1c266068b6d17a08b069243a458
matt nowakowski <mattnovak@fullsail.edu>
2013-02-14 13:37:04 -0500

    i finished the project.

--- Nowakowski_Matt_assignment2.js
+++ Nowakowski_Matt_assignment2.js
@@ -6,18 +6,20 @@
 // Story
 
 // variables
-var soldier = "Lt. Smith"
+var soldier = "Commander"
+var wars = true
+var air = "DuckaDucka"
 var numOfPlanes = "20"
 var squad1 = "Alpha",
 	squad2 = "Bravo",
 	squad3 = "Charlie",
-	squad4 = "Delta"
+	squad4 = "and Delta"
 ;
 var squads = [
 	"Alpha",
 	"Bravo",
 	"Charlie", 
-	"Delta"
+	"and Delta"
 ];
 
 // function
@@ -27,7 +29,55 @@ var planes = function(firstNumber,secondNumber){
 	return outcome;
 
 }
-// main code
-var planesLost = planes(20,7);
+var say = function(message) { console.log(message); };
 
-console.log(soldier + " we have lost " + planesLost + " planes since this war started.");
+var onePost = function(){
+var postNames = [ "north ridge", "south ridge", "east ridge", "west ridge"],
+	minPerPost = [10,10,10,10];
+for (var postNumber = 0; postNumber < postNames.length; postNumber++){
+	var postName = postNames[postNumber],
+		minThisPost = minPerPost[postNumber];
+		say("starting to clear " + postName + " for " + minThisPost + " minutes.");
+for (var min = 0; min < minThisPost; min += 5) {
+	var minRemain = minThisPost - min;
+	say(min + " done, we have " + minRemain + " to go " + soldier);
+	}
+	say(postName + " all clear " + soldier + " moving on.");
+ }
+
+};
+
+var attack = function(soldier, squad3) {
+	say(soldier + squad3 + " team can't make it base they're engaging the enemy.");
+	};
+var help = function(squad2, air) {
+	say("Send " + squad2 + " and 2 " + air + " to cover them from the air");
+	};
+
+
+
+	
+// main code
+
+if (wars === true){
+ true;say(soldier + " are planes are dropping like flies." + squads + " teams need to hurry back and take out the AA cannons.");
+	} else {
+   false;("none of the planes have gone down and " + squads + " team are playing volleyball.");
+}
+
+var planesLeft = planes(20,7);
+
+say(soldier + " we have lost " + planesLeft + " since " + squads + " team left to clear the posts");
+
+if (numOfPlanes === "20"){
+	say("Thats ok 7 planes should be more than enough to win this war with " + squads + " team.");
+	} else {
+	say ("we won't have enough resources to win even with " + squads + " team.");
+	
+}
+	say (soldier + " the squads are out clearing posts. get a sitrep and have them report in every 5 min.");
+onePost();
+say("all posts are clear " + soldier + " we're heading back to base");
+attack("Commander,"," Charlie");
+help("Bravo" , "DuckaDucka");
+say("to be continued");
