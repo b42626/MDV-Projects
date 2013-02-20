bcf20fa4b77c80c31c4e4e7578e8aca40e3eece3
matt nowakowski <mattnovak@fullsail.edu>
2013-02-20 11:50:08 -0500

    added json data

--- Nowakowski_Matt_assignment3.js
+++ Nowakowski_Matt_assignment3.js
@@ -1,1 +1,29 @@
 //alert("JavaScript works!");
+
+// Matt Nowakowski
+// Project 3
+// SDI 1302
+// War Game
+
+// local variables
+var game = "King of the Hill"
+var win = "true"
+var score = "100"
+
+
+
+
+
+
+// functions
+var say = function(message) { console.log(message); };
+
+var handleData = function (json) {
+	for (var i = 0; i < json.loadouts.length; i++){
+		var loadouts = json.loadouts[i];
+		//say("job: " + loadouts.job + ", weapon: " + loadouts.weapon + ", perk " + loadouts.perk );
+	};
+};
+handleData(json2);
+// Main Code
+
---
+++ json.js
@@ -0,0 +1,38 @@
+var json = {
+	"loadouts": {
+		"sniper": {
+			"weapon": "m21 scout",
+			"perk": "stealth"
+		},
+		"assault": {
+			"weapon": "ak47",
+			"perk": "medkit"
+		},
+		"support":{
+			"weapon": "RPK",
+			"perk": "ammo"
+		}
+	}
+};
+
+
+
+var json2 = {
+		"loadouts": [
+	{	
+		"job": "sniper",
+		"weapon": "m21 scout",
+		"perk": "stealth"
+	},
+	{
+		"job": "assault",
+		"weapon": "ak47",
+		"perk": "medkit"
+	},
+	{
+		"job": "support",
+		"weapon": "RPK",
+		"perk": "ammo"
+	}
+	]
+};
