08972323ea10b0ff56bebcb8722f3fbcde497709
matt nowakowski <mattnovak@fullsail.edu>
2013-02-27 13:01:49 -0500

    added second string fuction to script

--- Nowakowski_Matt_assignment4.js
+++ Nowakowski_Matt_assignment4.js
@@ -34,48 +34,6 @@ var checkNum = function (testNumber) {
     }
 }
 
-// 3. Title-case a string (split into words, then uppercase the first letter of each word)
-
-function fixCase(fixThis) {
- 
- 
-    String.prototype.toProperCase = function () {
- 
-        return this.replace(/\w\S*/g, function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-    };
-   
-    fixThis.toProperCase();
-    console.log(fixThis.toProperCase());
-}
-
-// 4. Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
-
-function changeSeparator(changeThis) {
-   
-    if (changeThis) {
-        return console.log("a/" + "b/" + "c/");
-    }
-}
-
-// 5. Find the number of hours or days difference between two dates.
-
-function daysPassed(date) {
-    var halo4 = date;
-    today = new Date(2013, 02, 27);
-    var one_day = 1000 * 60 * 60 * 12;
-    console.log(Math.ceil((today.getTime() - halo4.getTime()) / (one_day)) + " days have gone by since Halo 4 was released. " + date)
-}
-
-// 6. 
-
-
-
-
 
 checkNum ("123-456-7890");
 checkEmail("jubjub@gmail.com");
-fixCase("donkey kong");
-changeSeparator("a,b,c");
-daysPassed(new Date(2012, 10, 06));
